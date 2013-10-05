98deaf3e13fa5ba3336d0df4478aa9451ec0ba84
Carlos Alonso <c.pacheco.vilca@isur.edu.pe>
2013-10-05 18:35:50 -0500

    avance

---
+++ Agenda/Agenda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Agenda
+{
+    class Agenda
+    {
+        public void nuevo
+        {
+ 
+        }
+    }
+}
---
+++ Agenda/Persona.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Agenda
+{
+    class Persona
+    {
+        string Nombre;
+        string Apellido;
+        string Direccion;
+        int Telefono;
+
+        public Persona()
+        {
+            Nombre="";
+            Apellido="";
+            Direccion="";
+            Telefono=0;
+        }
+
+        public string nombre
+        {
+            get {return Nombre;}
+            set {Nombre = value;}
+        }
+
+        public string apellido
+        {
+            get { return Apellido; }
+            set { Apellido = value; }
+        }
+
+        public string direccion
+        {
+            get { return Direccion; }
+            set { Direccion = value; }
+        }
+
+        public int telefono
+        {
+            get { return Telefono; }
+            set { Telefono = value; }
+        }
+    }
+}
